9c2a79d9033db6fbe2fe1a156537fdad396aa737
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2024-03-06 20:03:14 +0600

    resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,59 @@
-import { useState } from 'react'
+import { useEffect, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const containerRef = useRef(null);
+  const textRef = useRef(null);
+
+
+  function resizeText() {
+    const container = containerRef.current;
+    const text = textRef.current;
+
+    const containerWidth = container.offsetWidth;
+    let min = 1;
+    let max = 2000;
+
+    while (min <= max) {
+      const mid = Math.floor((min + max) / 2);
+      text.style.fontSize = mid + "px";
+
+      if (text.offsetWidth <= containerWidth) {
+        min = mid + 1;
+      } else {
+        max = mid - 1;
+      }
+      console.log("I am running");
+    }
+    text.style.fontSize = max + "px";
+    console.log(max);
+  }
+
+  useEffect(() => {
+    resizeText();
+
+    window.addEventListener("resize", resizeText);
+
+    return () => {
+      window.removeEventListener("resize", resizeText);
+    };
+  }, []);
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
+    <div ref={containerRef} className='flex h-screen w-full items-center overflow-hidden bg-slate-950'>
+      <div className='absolute top-5 flex gap-5 w-full'  ref={textRef} >
+        <a href="https://developer-nahid.web.app/" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://developer-nahid.web.app/" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <span ref={textRef} className='absolute left-0 mx-auto my-auto whitespace-nowrap text-center font-bold uppercase text-slate-700'>I Fit Myself Here</span>
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -27,8 +63,8 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </p> */}
+    </div>
   )
 }
 
